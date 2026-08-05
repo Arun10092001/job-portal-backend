656ac8c1ff482e7ee24f644b73bbff745e4c6a12
Arun10092001 <gokularun529@gmail.com>
2026-08-05 14:34:53 +0530

    Update Django settings for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = "django-insecure-$#k!7l48r^z2v+$1m=g%s&8a2jjjd=4+x2cl8^^t@qnc@hkr$1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
